c15367f6f537f80c4544f8e25fb2ae20f0fb8245
Jackie Kelly <jacqueline.m.k06@gmail.com>
2020-03-01 18:49:55 -0800

    working on search

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,7 +19,21 @@ public class MyMiniSearchEngine {
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
-        //homework
+        indexes = new HashMap<>();
+        for(int doc = 0; doc < texts.size(); doc++) {
+            String[] tokens = texts.get(doc).split(" ");
+
+            for(int wordKey = 0; wordKey < tokens.length; wordKey++) {
+                List<List<Integer>> currentWordLocations = new ArrayList<>();
+                for(int i = 0; i < texts.size(); i++) {
+                    currentWordLocations.add(new ArrayList<>());
+                }
+                if(!indexes.containsKey(tokens[wordKey])){
+                    indexes.put(tokens[wordKey], currentWordLocations);
+                }
+                indexes.get(tokens[wordKey]).get(doc).add(wordKey);
+            }
+        }
     }
 
     // search(key) return all the document ids where the given key phrase appears.
@@ -26,6 +41,13 @@ public class MyMiniSearchEngine {
     // return an empty list if search() finds no match in all documents.
     public List<Integer> search(String keyPhrase) {
         // homework
-        return new ArrayList<>(); // place holder
+        List<Integer> locations = new ArrayList<>();
+        for(int i = 0; i < indexes.get(keyPhrase).size(); i++) {
+            if (indexes.get(keyPhrase).get(i).size() > 0)
+            locations.add(i);
+        }
+        //indexes.get(keyPhrase);
+
+        return locations;
     }
 }
